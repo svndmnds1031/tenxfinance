836261a0bdd71a46d5103beac9fe04e61e1031c6
Patricio Landeros <665971cplp@gmail.com>
2026-04-28 23:05:08 -0600

    Initial Commit

---
+++ tenxfinance/tenxfinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  tenxfinanceApp.swift
+//  tenxfinance
+//
+//  Created by Patricio Landeros on 28/04/26.
+//
+
+import SwiftUI
+
+@main
+struct tenxfinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
